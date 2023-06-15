49fc5f88159aef61a1c8caaeea5a40b0f15ee4ed
Lorenzo O. Zimbres <97912499+LoriaLawrenceZ@users.noreply.github.com>
2023-06-15 18:21:06 +0000

    deletando desnecessario

--- js/secretaria2.js
+++
@@ -1,4 +0,0 @@
-//-----==VARIÁVEIS==-----
-const cardGen = document.querySelectorAll("section");
-const voltarGen = document.querySelectorAll(".voltar");
-const cCardGen = document.querySelectorAll(".cCard");
